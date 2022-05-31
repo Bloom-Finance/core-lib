06adcc48750d7834cb4a3669812f5b37d3df567c
Leonardo Leenen <leonardoleenen@gmail.com>
2022-05-31 12:30:23 -0300

    Fix Hocks

--- common/hocs/preFetch/orders.tsx
+++ common/hocs/preFetch/orders.tsx
@@ -1,21 +1,22 @@
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import Loader from '../../../../ui-lib/src/components/loader/index'
-import { OrdersManager } from '../../../services/order-old.service'
+
 import { ErrorHandler } from '../../helpers/errorHandler'
 import { useEffect } from 'react'
 import _ from 'lodash'
+import { orderService } from '../../../services/order.service'
 const withPreFetch = <P extends object>(Component: any) => {
     const PreFetch = (props: any) => {
         const router = useRouter()
         const { id } = router.query
         const [order, setOrder] = useState<Order>()
-        const ordersManager = new OrdersManager()
+
         const getData = async () => {
             try {
-                const order = await ordersManager.getOrderById(id as string)
+                const order = await orderService.getOrder(id as string)
                 if (_.isEmpty(order)) router.push('/dashboard')
-                setOrder(order)
+                if (order) setOrder(order)
             } catch (error) {
                 new ErrorHandler(error)
             }
